b1c7e2b73d90ffa998ad028854fbc4f597931352
xuzhengpu <xuzhengpu1995@outlook.com>
2018-12-02 18:16:04 +0800

    实现模型显示

--- opengl/Mesh.cpp
+++ opengl/Mesh.cpp
@@ -66,18 +66,15 @@ void Mesh::setupMesh()
 	glGenBuffers(1, &EBO);
 	//°ó¶¨VAO
 	glBindVertexArray(VAO);
-	// load data into vertex buffers
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// A great thing about structs is that their memory layout is sequential for all its items.
-	// The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-	// again translates to 3/2 floats which translates to a byte array.
+
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-	// set the vertex attribute pointers
-	// vertex Positions
+	
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 	// vertex normals
--- opengl/main.cpp
+++ opengl/main.cpp
@@ -16,25 +16,24 @@ float pitch = 0;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-// settings
+// 视口大小
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
+//相机的三个向量 位置 看向 上向量
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 int main()
 {
-	// glfw: initialize and configure
-	// ------------------------------
+	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     stbi_set_flip_vertically_on_load(true);
 #ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
 #endif
 
 	// glfw window creation
@@ -57,6 +56,8 @@ int main()
 		return -1;
 	}
 
+	//开启深度测试，让模型看起来更有立体感
+    glEnable(GL_DEPTH_TEST);
 	// build and compile our shader program
 	// ------------------------------------
 	//Shader ourShader("shader_vs.txt","shader_fs.txt");
@@ -66,7 +67,7 @@ int main()
 	// load models
 	// -----------
 	Model ourModel("nanosuit/nanosuit.obj");
-
+	
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -82,20 +83,20 @@ int main()
 		// 设置背景颜色
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		
 		// 着色器
 		ourShader.use();
-
+		//初始化三个变换矩阵
 		glm::mat4 projection = glm::mat4(1.0f);    //投影矩阵
 		glm::mat4 view = glm::mat4(1.0f);          //摄像机矩阵
 		glm::mat4 model = glm::mat4(1.0f);         //世界矩阵
 		
-
-		//glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		//glm::mat4 view = camera.GetViewMatrix();
-		//ourShader.setMat4("projection", projection);
-		//ourShader.setMat4("view", view);
+		//使用lookat 生成相机矩阵
 		view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
+		//将矩阵传入着色器中
 		ourShader.setMat4("view", view);
+
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		ourShader.setMat4("projection", projection);
 
@@ -105,50 +106,16 @@ int main()
 
 		//绘制模型
 		ourModel.Draw(ourShader);
-		//float radius = 10.0f;
-		///*float camX = sin(glfwGetTime()) * radius;
-		//float camZ = cos(glfwGetTime()) * radius;
-		//view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//ourShader.setMat4("view", view);*/
-		//view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
-		//ourShader.setMat4("view", view);
-		//projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		//ourShader.setMat4("projection", projection);
-		//// render boxes
-		//glBindVertexArray(VAO);
-		//for (unsigned int i = 0; i < 10; i++)
-		//{
-		//	// calculate the model matrix for each object and pass it to shader before drawing
-		//	
-		//	model = glm::translate(model, cubePositions[i]);
-		//	float angle = 20.0f * i;
-		//	model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-		//	ourShader.setMat4("model", model);
-
-		//	glDrawArrays(GL_TRIANGLES, 0, 36);
-		//}
-
-		/*glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
-	/*glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);*/
-
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+// 键盘控制的回调函数
 void processInput(GLFWwindow *window)
 {
 	float cameraSpeed = 2.5f * deltaTime;
@@ -195,11 +162,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
 	cameraFront = glm::normalize(front);
 }
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+// 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
+	
 	glViewport(0, 0, width, height);
 }
